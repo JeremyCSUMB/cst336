7ac790946c60e90396bf4255be1f127e631a3924
Jeremy S <jeshaw@csumb.edu>
2018-08-27 22:35:37 +0000

    updating index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 // Press the 'Run' button on the top to start the web server,
 // then click the URL that is emitted to the Output tab of the console
 
-echo "Jeremy Shaw's CST336 site";
+echo "<h1>Jeremy Shaw's CST336 site</h1>";
 
 ?>
 </body>
