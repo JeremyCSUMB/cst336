8e8c5e5ec9935691662e89d4a84a16939b830829
Jeremy S <jeshaw@csumb.edu>
2018-08-29 22:04:58 +0000

    adding lab 1 files

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
-<html><body>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Jeremy's Web Portfolio</title>
+    </head>
+    <body>
 <?php
 // A simple web site in Cloud9 that runs through Apache
 // Press the 'Run' button on the top to start the web server,
@@ -7,5 +12,5 @@
 echo "<h2>Jeremy Shaw's CST336 site</h2>";
 
 ?>
-</body>
+    </body>
 </html>
