00334875759c73964c608223e82027df2990506e
Jeremy S <jeshaw@csumb.edu>
2018-09-22 22:06:52 +0000

    Finishing up lab2

--- labs/lab2/index.php
+++ labs/lab2/index.php
@@ -18,5 +18,20 @@ include "inc/functions.php";
                 <input type="submit" value="Spin!"/>
             </form>
         </div>
+        <br />
+        <br />
+        <br />
+        <footer>
+            <hr>
+            CST 336: Internet Programming &copy; 2018 Jeremy Shaw <br />
+            <strong>Disclaimer:</strong> The information in this website is fake.
+            <br />
+            It is for educational purposes only.
+            <br />
+            
+            <img src="../../img/otter.jpg" alt="CSUMB logo">
+            <br />
+            <img src="../../img/buddyprogram.png" alt="Buddy Program">
+        </footer>
     </body>
 </html>
