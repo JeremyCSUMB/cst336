525aab8000ba232476bf1a17a617e70e29a7d729
Jeremy S <jeshaw@csumb.edu>
2018-10-28 02:21:34 +0000

    modified lab6

--- labs/lab6/index.php
+++ labs/lab6/index.php
@@ -12,23 +12,19 @@
             echo "<option value='".$record['catId']."'>" . $record['catName'] . "</option>";
         }
     }
-    
     function displaySearchResults() {
         global $dbConn;
         $namedParameters = array();
         $product = $_GET['productName'];
         $sql = "SELECT * FROM om_product WHERE 1";
-        
         if (!empty($product)){
              $sql .=  " AND productName LIKE :product";
              $namedParameters[':product'] = "%$product%";
         }
-        
         if (!empty($_GET['category'])){
              $sql .=  " AND catId =  :category";
               $namedParameters[':category'] = $_GET['category'] ;
         }
-    
         if (isset($_GET['orderBy'])) {
             if ($_GET['orderBy'] == "productPrice") {
                 $sql .= " ORDER BY price";
@@ -36,7 +32,6 @@
                   $sql .= " ORDER BY productName";
             }
         }
-    
         $stmt = $dbConn->prepare($sql);
         $stmt->execute($namedParameters);
         $records = $stmt->fetchAll(PDO::FETCH_ASSOC);  
@@ -56,29 +51,32 @@
     <head>
         <title>OtterMart Product Search</title>
         <link href="css/styles.css" rel="stylesheet" type="text/css" />
+        <style>
+            @import url('https://fonts.googleapis.com/css?family=Open+Sans');
+        </style>
     </head>
     <body>
         <div>
             <h1> OtterMart Product Search </h1>
             <form>
                 Product: <input type="text" name="productName" />
-                <br>
+                <br><br>
                 Category:
                     <select name="category">
                         <option value="">Select One</option>
                         <?=displayCategories()?>
                     </select>
-                <br>
+                <br><br>
                 Price: From <input type="text" name="priceFrom" size="7"/>
                        To <input type="text" name="priceFrom" size="7"/>
-                <br>
+                <br><br>
                 Order result by:
-                <br>
+                <br><br>
                 
                 <input type="radio" name="orderBy" value="productPrice"/> Price <br>
                 <input type="radio" name="orderBy" value="productName"/> Name <br>
                 
-                <br><br>
+                <br>
                 <input type="submit" value="Search" name="searchForm" />
             </form>
             
