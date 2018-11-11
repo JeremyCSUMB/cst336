662233909a82b8925b937e1e06a315902b87e591
Jeremy S <jeshaw@csumb.edu>
2018-11-11 05:08:54 +0000

    Added lab 7

--- labs/lab7/loginProcess.php
+++ labs/lab7/loginProcess.php
@@ -6,15 +6,18 @@ $dbConn = startConnection("ottermart");
 
 $username = $_POST['username'];
 $password = sha1($_POST['password']);
-
+                 
 $sql = "SELECT * FROM om_admin
-                 WHERE username = '$username' 
-                 AND  password = '$password'";
-$stmt = $dbConn->prepare($sql);
-$stmt->execute();
-$record = $stmt->fetch(PDO::FETCH_ASSOC); //we're expecting just one record
+                 WHERE username = :username 
+                 AND  password = :password ";                 
+$np = array();
+$np[':username'] = $username;
+$np[':password'] = $password;
 
-//print_r($record);
+$stmt = $dbConn->prepare($sql);
+$stmt->execute($np);
+$record = $stmt->fetch(PDO::FETCH_ASSOC);
+
 
 if (empty($record)) {
     
@@ -22,7 +25,7 @@ if (empty($record)) {
 } else {
    
    $_SESSION['adminFullName'] = $record['firstName'] .  "   "  . $record['lastName'];
-   header('Location: admin.php'); //redirects to another program
+   header('Location: admin.php');
     
 }
 
