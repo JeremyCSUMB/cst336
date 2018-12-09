766c5fe14ea56c81c291bbe47f37a5cd10bc9bf4
Jeremy S <jeshaw@csumb.edu>
2018-12-09 04:57:19 +0000

    Added some files

--- labs/lab10/inc/footer.php
+++ labs/lab10/inc/footer.php
@@ -2,7 +2,7 @@
         <footer>
             
             Disclaimer: The information on this site is fake.<br>
-            &copy; Lara 2019
+            &copy; Shaw 2018
             
         </footer>
         
